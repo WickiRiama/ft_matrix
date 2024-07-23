57b69452731b82eed8710d4dad46d4e1b37124b7
Margaux RIANT <mriant@student.42.fr>
2024-07-23 17:55:59 +0200

    Ex02: Matrix: add lerp function

--- ex02/includes/lerp.tpp
+++ ex02/includes/lerp.tpp
@@ -3,7 +3,8 @@
 
 #include <cmath>
 
-#include "Vector_lin_cmb.hpp"
+#include "Vector.hpp"
+#include "Matrix.hpp"
 
 template <typename K>
 K lerp(K const u, K const v, float t)
@@ -26,5 +27,25 @@ Vector<K> lerp(Vector<K> const &u, Vector<K> const &v, float t)
 	}
 	return result;
 }
+
+template <typename K>
+Matrix<K> lerp(Matrix<K> const &u, Matrix<K> const &v, float t)
+{
+	std::pair<size_t, size_t> shape = u.getShape();
+	if (shape != v.getShape())
+	{
+		throw std::length_error("Matrices with different sizes");
+	}
+	Matrix<K> result(u);
+	for (size_t row = 0; row < shape.first; row++)
+	{
+		for (size_t col = 0; col < shape.second; col++)
+		{
+			result[row][col] = fma(t, v[row][col] - u[row][col], u[row][col]);
+		}
+	}
+	return result;
+}
+
 
 #endif
--- ex02/srcs/test_lerp.cpp
+++ ex02/srcs/test_lerp.cpp
@@ -2,6 +2,8 @@
 #include <sstream>
 #include <iostream>
 
+#include <cmath>
+
 #include "lerp.hpp"
 
 Test(Lerp, floatSchool)
@@ -10,6 +12,19 @@ Test(Lerp, floatSchool)
 	cr_expect(isEqual(lerp(0., 1., 1.), 1.0));
 	cr_expect(isEqual(lerp(0., 1., 0.5), 0.5));
 	cr_expect(isEqual(lerp(21., 42., 0.3), 27.3));
+}
+
+Test(Lerp, floatReverse)
+{
+	cr_expect(isEqual(lerp(1., 0., 0.), 1.0));
+	cr_expect(isEqual(lerp(1., 0., 1.), 0.0));
+	cr_expect(isEqual(lerp(1., 0., 0.5), 0.5));
+	cr_expect(isEqual(lerp(42., 21., 0.3), 35.7));
+}
+
+Test(Lerp, extrapolation)
+{
+	cr_expect(isEqual(lerp(0., 1., 1.5), 1.5));
 }
 
 Test(Lerp, intLerp)
@@ -25,6 +40,13 @@ Test(Lerp, vectorSchool)
 	cr_expect(lerp(Vector<float>({2., 1.}), 
 			  Vector<float>({4., 2.}), 0.3) == Vector<float>({2.6, 1.3}));
 }
+
+Test(Lerp, vectorReverse)
+{
+	cr_expect(lerp(Vector<float>({4., 2.}), 
+			  Vector<float>({2., 1.}), 0.3) == Vector<float>({3.4, 1.7}));
+}
+
 
 Test(Lerp, vectorEmpty)
 {
@@ -44,4 +66,40 @@ Test(Lerp, vectorSizeError)
 		ss << e.what();
 	}
 	cr_expect(ss.str() == "Vectors with different sizes");
+}
+
+Test(Lerp, matrixSchool)
+{
+	cr_expect(lerp(Matrix<float>({{2., 1.}, {3., 4.}}), 
+			  Matrix<float>({{20., 10.}, {30., 40.}}), 0.5) \
+			  == Matrix<float>({{11., 5.5}, {16.5, 22.}}));
+}
+
+Test(Lerp, matrixReverse)
+{
+	cr_expect(lerp(Matrix<float>({{20., 10.}, {30., 40.}}), 
+			  Matrix<float>({{2., 1.}, {3., 4.}}), 0.5) \
+			  == Matrix<float>({{11., 5.5}, {16.5, 22.}}));
+}
+
+Test(Lerp, matrixEmpty)
+{
+	cr_expect(lerp(Matrix<float>(), Matrix<float>(), 0.5) \
+			  == Matrix<float>());
+}
+
+Test(Lerp, matrixSizeError)
+{
+	std::stringstream ss("");
+	std::vector<float> v(1, 1.);
+	std::vector<std::vector<float>> m(1, v);
+	try
+	{
+		Matrix<float> result = lerp(Matrix<float>({m}), Matrix<float>({{1.}, {2.}}), 0.5);
+	}
+	catch(const std::length_error& e)
+	{
+		ss << e.what();
+	}
+	cr_expect(ss.str() == "Matrices with different sizes");
 }

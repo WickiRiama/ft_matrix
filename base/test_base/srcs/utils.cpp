b8aacabfce866329dd2b2930a3fb79d3da51d803
Margaux RIANT <mriant@student.42.fr>
2024-07-24 18:20:05 +0200

    Utils: use a system constant to choose the precision of isEqual function

--- base/test_base/srcs/utils.cpp
+++ base/test_base/srcs/utils.cpp
@@ -1,7 +1,13 @@
-#include "Matrix.hpp"
+#include <limits>
+#include <iostream>
 
 bool isEqual(float const &a, float const &b)
 {
-	float epsilon = 0.000001;
+	// std::numeric_limits<float>::digits10 : number of decimal digits that can be represented without change
+	float epsilon = 1;
+	for (int i = 0; i < std::numeric_limits<float>::digits10; i++)
+	{
+		epsilon /= 10;
+	}
 	return (a <= b + epsilon && a >= b - epsilon);
 }

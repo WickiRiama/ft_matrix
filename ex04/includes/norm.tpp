57fbecb1414ca5df742b072f64533de559ef853e
Margaux RIANT <mriant@student.42.fr>
2024-07-24 18:21:03 +0200

    Ex04: Vector: Add Manathan norm method (norm_1)

--- ex04/includes/norm.tpp
+++ ex04/includes/norm.tpp
@@ -1,5 +1,27 @@
 #ifndef NORM_TPP
 #define NORM_TPP
 
+#include <cmath>
+
+#include "Vector.hpp"
+
+template <typename K>
+float Vector<K>::norm_1(void)
+{
+	size_t size = this->getSize();
+	float result = 0;
+	for (size_t i = 0; i < size; i++)
+	{
+		if ((*this)[i] < 0)
+		{
+			result = fma(-1., (*this)[i], result);
+		}
+		else
+		{
+			result += (*this)[i];
+		}
+	}
+	return result;
+}
 
 #endif
--- ex04/srcs/test_norm.cpp
+++ ex04/srcs/test_norm.cpp
@@ -4,7 +4,26 @@
 
 #include "norm.hpp"
 
-Test(Norm, empty)
+Test(Norm, school1)
 {
-	cr_assert(1 == 1);
+	Vector<float> u({0., 0., 0.});
+	cr_assert(isEqual(u.norm_1(), 0.0));
+}
+
+Test(Norm, school2)
+{
+	Vector<float> u({1., 2., 3.});
+	cr_assert(isEqual(u.norm_1(), 6.0));
+}
+
+Test(Norm, school3)
+{
+	Vector<float> u({-1., -2.});
+	cr_assert(isEqual(u.norm_1(), 3.0));
+}
+
+Test(Norm, emptyVector)
+{
+	Vector<float> u;
+	cr_assert(isEqual(u.norm_1(), 0.));
 }
